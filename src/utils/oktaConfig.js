39a1c1ef077813ab108f1c27218a3164110b1e6a
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-10-28 13:08:49 -0400

    hard coded env variables since i dont have env file

--- src/utils/oktaConfig.js
+++ src/utils/oktaConfig.js
@@ -1,7 +1,7 @@
 const config = {
-  issuer: process.env.REACT_APP_OKTA_ISSUER_URI,
+  issuer: 'https://example-882474.okta.com', //process.env.REACT_APP_OKTA_ISSUER_URI,
   redirectUri: window.location.origin + '/implicit/callback',
-  clientId: process.env.REACT_APP_CLIENT_ID,
+  clientId: 'example', //process.env.REACT_APP_CLIENT_ID,
   pkce: true,
   scopes: ['openid', 'email', 'profile'],
 };
