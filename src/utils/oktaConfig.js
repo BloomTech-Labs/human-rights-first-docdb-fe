3092570ae15e38b277b8de6056104a105b3b7a81
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-10-29 12:03:22 -0400

    fixed okta config error

--- src/utils/oktaConfig.js
+++ src/utils/oktaConfig.js
@@ -1,7 +1,7 @@
 const config = {
-  issuer: 'https://example-882474.okta.com', //process.env.REACT_APP_OKTA_ISSUER_URI,
+  issuer: process.env.REACT_APP_OKTA_ISSUER_URI,
   redirectUri: window.location.origin + '/implicit/callback',
-  clientId: 'example', //process.env.REACT_APP_CLIENT_ID,
+  clientId: process.env.REACT_APP_CLIENT_ID,
   pkce: true,
   scopes: ['openid', 'email', 'profile'],
 };
