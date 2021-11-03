f41f3723407c2c1d11f807eba17ef2fca441ca46
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-11-03 13:59:12 -0400

    Updated some styling for LandingCard.js.

--- src/components/pages/Landing/LandingCard.js
+++ src/components/pages/Landing/LandingCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Tag, Grid } from 'antd';
+import { Card, Tag, Row, Col } from 'antd';
 import { StarOutlined, StarFilled, ArrowsAltOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
@@ -11,11 +11,22 @@ function LandingCard(props) {
       title={<ArrowsAltOutlined />}
       cover={<img src={preview} alt={title} />}
       extra={<StarOutlined />}
+      style={{ width: 300 }}
+      bodyStyle={{ padding: '12px' }}
     >
-      <Meta title={title}></Meta>
-      {tags.map(tag => {
-        return <Tag key={tag}> {tag} </Tag>;
-      })}
+      <Meta
+        title={title}
+        style={{ textAlign: 'center', marginBottom: '10px' }}
+      />
+      <Row>
+        {tags.map(tag => {
+          return (
+            <Col span={0} style={{ display: 'flex', justifyContent: 'center' }}>
+              <Tag key={tag}> {tag} </Tag>
+            </Col>
+          );
+        })}
+      </Row>
     </Card>
   );
 }
