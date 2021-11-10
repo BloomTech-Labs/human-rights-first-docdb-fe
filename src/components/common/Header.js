006d4b128f3df69d638b729d7296151615535cb4
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-11-10 11:42:50 -0600

    added bookmarks btn

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Layout } from 'antd';
 import Search from 'antd/es/input/Search';
-import { Avatar } from 'antd';
+import { Avatar, Layout, Button } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
 import 'antd/dist/antd.css';
 import './header.css';
 import logo from '../../assets/HRF_Logo.webp';
 import { useOktaAuth } from '@okta/okta-react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 const { Header } = Layout;
 
 function MainHeader(props) {
   const { logout } = useOktaAuth().authService;
   const { pathname } = useLocation();
+
   if (pathname === '/login') return null;
 
   return (
@@ -25,6 +24,11 @@ function MainHeader(props) {
         <Button onClick={logout} className="logout_button" type="default">
           Logout
         </Button>
+        <Link to="/">
+          <Button className="bookmark_button" type="default">
+            Bookmarks
+          </Button>
+        </Link>
         <Avatar className="avatars" size={45} icon={<UserOutlined />} />
       </Header>
     </Layout>
