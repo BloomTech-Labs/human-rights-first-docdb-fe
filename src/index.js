215cee204c415c11d33980927ea71d043c596243
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-11-12 16:23:17 -0600

    updated get ds function to be more flexible and added url to enviornment varibale

--- src/index.js
+++ src/index.js
@@ -36,13 +36,9 @@ ReactDOM.render(
 );
 
 function App() {
-  // The reason to declare App this way is so that we can use any helper functions we'd need for business logic, in our case auth.
-  // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
   const history = useHistory();
 
   const authHandler = () => {
-    // We pass this to our <Security /> component that wraps our routes.
-    // It'll automatically check if userToken is available and push back to login if not :)
     history.push('/login');
   };
 
