5f049d3f03cab18ddb7eab0830fe510381f35480
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-11-14 20:59:32 -0500

    Clicking a card redirects user to their Box account to view documents.

--- src/components/pages/Landing/LandingCard.js
+++ src/components/pages/Landing/LandingCard.js
@@ -12,42 +12,43 @@ function LandingCard(props) {
   const { name, url, tags, favorited } = props;
 
   return (
-    <Card
-      name={<ArrowsAltOutlined rotate={90} />}
-      // cover={<img src={null} alt={name} />}
-      cover={
-        <img
-          src={`https://simg.nicepng.com/png/small/370-3706418_change-of-information-document-icon-free-vector.png`}
-          alt={name}
+    <div onClick={() => window.open(url)}>
+      <Card
+        name={<ArrowsAltOutlined rotate={90} />}
+        // cover={<img src={null} alt={name} />}
+        cover={
+          <img
+            src={`https://simg.nicepng.com/png/small/370-3706418_change-of-information-document-icon-free-vector.png`}
+            alt={name}
+          />
+        }
+        extra={
+          favorited ? (
+            <img
+              src={bookmarkFilled}
+              alt="bookmark filled"
+              width="50"
+              data-testid="filled-bookmark"
+            />
+          ) : (
+            <img
+              src={bookmarkOutlined}
+              alt="bookmark outlined"
+              width="50"
+              data-testid="outlined-bookmark"
+            />
+          )
+        }
+        style={{ width: 300 }}
+        bodyStyle={{ padding: '12px' }}
+      >
+        <Meta
+          title={name}
+          style={{ textAlign: 'center', marginBottom: '10px' }}
         />
-      }
-      anchor={<url src={url} />}
-      extra={
-        favorited ? (
-          <img
-            src={bookmarkFilled}
-            alt="bookmark filled"
-            width="50"
-            data-testid="filled-bookmark"
-          />
-        ) : (
-          <img
-            src={bookmarkOutlined}
-            alt="bookmark outlined"
-            width="50"
-            data-testid="outlined-bookmark"
-          />
-        )
-      }
-      style={{ width: 300 }}
-      bodyStyle={{ padding: '12px' }}
-    >
-      <Meta
-        title={name}
-        style={{ textAlign: 'center', marginBottom: '10px' }}
-      />
-      <Tags tagArray={tags} size={8} />
-    </Card>
+        <Tags tagArray={tags} size={8} />
+      </Card>
+    </div>
   );
 }
 
