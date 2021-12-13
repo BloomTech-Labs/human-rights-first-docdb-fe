e8bd8790e3f7a5720837999ca0158f457c533957
Bobby <monkeyeden.rjt@gmail.com>
2021-12-13 16:48:44 -0500

    commented out isFavorite from LandingListCard

--- src/components/pages/Landing/LandingListCard.js
+++ src/components/pages/Landing/LandingListCard.js
@@ -92,7 +92,7 @@ LandingCardList.propTypes = {
   name: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   tags: PropTypes.arrayOf(PropTypes.string),
-  isFavorite: PropTypes.bool.isRequired,
+  // isFavorite: PropTypes.bool.isRequired,
 };
 const mapStateToProps = state => ({
   bookmarkedDocs: state.bookmarkedDocs,
