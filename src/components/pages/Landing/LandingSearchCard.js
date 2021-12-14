b784074d5a8a040bec17bd4805315aa2cdde6749
someilam <lamsomeiamy@gmail.com>
2021-12-13 17:29:25 -0800

    aadded styling to landing page if there is no bookmarks

--- src/components/pages/Landing/LandingSearchCard.js
+++ src/components/pages/Landing/LandingSearchCard.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import { Input } from 'antd';
+import { searchDocs } from '../../../state/actions';
+import { useOktaAuth } from '@okta/okta-react';
+import { connect } from 'react-redux';
+import logo2 from '../../../assets/HRF_Logo2.png';
 
 const { Search } = Input;
 
-const onSearch = value => console.log(value);
+const landingSearchStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+};
 
 function LandingSearchCard(props) {
+  const { searchDocs } = props;
+  const { authState } = useOktaAuth();
+
+  const onSearch = value => {
+    if (!value) return alert('Search bar cannot be empty');
+    searchDocs(value, authState);
+  };
+
   return (
-    <div>
+    <div style={{ ...landingSearchStyle }}>
+      <img
+        src={logo2}
+        className="header_img"
+        alt="HRF logo"
+        style={{ margin: '10rem 0 2rem 0' }}
+      />
       <Search
         placeholder="input search text"
         onSearch={onSearch}
-        style={{ width: 200 }}
+        style={{ width: 500 }}
       />
     </div>
   );
 }
 
-export default LandingSearchCard;
+export default connect(null, { searchDocs })(LandingSearchCard);
