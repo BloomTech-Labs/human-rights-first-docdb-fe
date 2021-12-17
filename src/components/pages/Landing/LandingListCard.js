0d64c615f3837514f50cb6c17f42d1cc312814da
Bobby <monkeyeden.rjt@gmail.com>
2021-12-17 00:32:00 -0500

    removed the isRequired from isFavorite in LandingListCard

--- src/components/pages/Landing/LandingListCard.js
+++ src/components/pages/Landing/LandingListCard.js
@@ -96,7 +96,7 @@ LandingCardList.propTypes = {
   name: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   tags: PropTypes.arrayOf(PropTypes.string),
-  isFavorite: PropTypes.bool.isRequired,
+  isFavorite: PropTypes.bool,
 };
 const mapStateToProps = state => ({
   bookmarkedDocs: state.bookmarkedDocs,
