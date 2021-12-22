9dae433ed9e55dc2d418178b498e0319f80cf86f
Bobby <monkeyeden.rjt@gmail.com>
2021-12-22 17:00:07 -0500

    cleaned up TagsList getting rid of size no longer needed

--- src/components/common/Tags/TagsList.js
+++ src/components/common/Tags/TagsList.js
@@ -1,51 +1,15 @@
 import React from 'react';
-import { Tag, Popover, Row, Col } from 'antd';
-import { EllipsisOutlined } from '@ant-design/icons';
+import { Row } from 'antd';
 import ColTagList from '../ColTag/ColTagList';
 import '../ColTag/ColTagList.css';
 
 function TagsList(props) {
-  const { size, tagArray } = props;
-
-  if (tagArray.length <= size)
-    return (
-      <Row>
-        {tagArray.map((tag, index) => (
-          <ColTagList classList="colTag" tag={tag} key={index} />
-        ))}
-      </Row>
-    );
-
-  const shownTags = tagArray.slice(0, size - 1);
-  const hiddenTags = tagArray.slice(size - 1, tagArray.length);
-
+  const { tagArray } = props;
   return (
-    <Row className="tagsListRow">
-      {shownTags.map((tag, index) => (
+    <Row>
+      {tagArray.map((tag, index) => (
         <ColTagList classList="colTag" tag={tag} key={index} />
       ))}
-      <Col
-        style={{
-          display: 'flex',
-          margin: '0 auto 2% auto',
-        }}
-      >
-        <Popover
-          title="Tags cont."
-          classList="colTag"
-          content={
-            <Row>
-              {hiddenTags.map((tag, index) => (
-                <ColTagList tag={tag} key={index} />
-              ))}
-            </Row>
-          }
-        >
-          <Tag>
-            <EllipsisOutlined />
-          </Tag>
-        </Popover>
-      </Col>
     </Row>
   );
 }
