0a6c0e34b9430803aca41df17bd18c8ee8fef565
Sarah D Herren <herren.sarah@icloud.com>
2021-12-22 17:24:01 -0600

    updates card borders & hoverability

--- src/components/pages/Landing/LandingCard.js
+++ src/components/pages/Landing/LandingCard.js
@@ -96,7 +96,7 @@ function LandingCard(props) {
           style={{
             width: 300,
             marginBottom: '17%',
-            border: '3px outset #DAC6B2',
+            border: '1px solid #DAC6B2',
           }}
           headStyle={{ height: 35, padding: 0 }}
           bodyStyle={{ padding: 12 }}
@@ -139,8 +139,10 @@ function LandingCard(props) {
             display: 'flex',
             justifyContent: 'left',
             marginBottom: '3%',
+            border: '1px solid #DAC6B2',
           }}
           bodyStyle={{ overflow: 'auto', whiteSpace: 'normal' }}
+          hoverable={true}
         >
           <img
             src={isFavorite ? BookmarkFilled : BookmarkOutlined}
